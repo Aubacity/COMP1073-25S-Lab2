80f82fcf0715627140afae2f31eca9241cd9b33a
S. Joseph C <20764341+Aubacity@users.noreply.github.com>
2025-07-02 20:27:05 -0400

    Overwrite merge with Afia

--- js/index.js
+++ js/index.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
 
-function ApplicationProfile(appFullName, appLocation, appTargetRole, appDesiredExperience, appEducation, appSkills, appCertifications, appLanguageSpoken, appPortfolioURL, appRelocation, appRemotePreferred, appSoftSkills, appInternship) {
-    this.appFullName = null;
-    this.appLocation = appLocation;
-    this.appTargetRole = null;
-    this.appDesiredExperience = null;
-    this.appEducation = null;
-    this.appSkills = null;
-    this.appCertifications = null;
-    this.appLanguageSpoken = null;
-    this.appPortfolioURL = null;
-    this.appRelocation = null;
-    this.appRemotePreferred = null;
-    this.appSoftSkills = null;
-    this.appInternship = null;
-}
-
-=======
 // Applicant Constructor to define the structure of the job applicant Liam
 
 function ApplicantDetails (name, location, targetRole, yearsExperience, education, skills, certification, languagesSpoken, portfolio, willingToRelocate, remoteWorkPreference, softSkills, openToInternships) {
@@ -38,4 +20,3 @@ function ApplicantDetails (name, location, targetRole, yearsExperience, educatio
 // Creating an Object Using the Applicant Constructor
 const liam = new ApplicantDetails("Liam O'Reily", "Halifax, NS", "Marine Technician", 4, "Diploma in Marine Technology, Nova Scotia Community College"["Sonar operation", "Engine repair", "Navigation systems"], ["Marine Safety Training"], ["English"], null, true, false, ["Teamwork", "Adaptability"], false
 );
->>>>>>> 87065a79f9a26ada69063ad145e85dab8b3de8ba
