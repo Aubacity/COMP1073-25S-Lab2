f984ef50a868806d68bd4b3c997fa0ca3d9a9a62
S. Joseph C <20764341+Aubacity@users.noreply.github.com>
2025-07-02 21:10:01 -0400

    It's always the commas

--- js/index.js
+++ js/index.js
@@ -15,14 +15,14 @@ function ApplicantDetails (name, location, targetRole, yearsExperience, educatio
     this.remoteWorkPreference = remoteWorkPreference;
     this.softSkills = softSkills;
     this.openToInternships = openToInternships;
-    this.displayDetails = function() {
+    this.describe = function() {
         return `Name: ${this.name}, Location: ${this.location}, Target Role: ${this.targetRole}, Years of Experience: ${this.yearsExperience}, Education: ${this.education}, Skills: ${this.skills.join(", ")}, Certification: ${this.certification.join(", ")}, Languages Spoken: ${this.languagesSpoken.join(", ")}, Portfolio: ${this.portfolio ? this.portfolio : "N/A"}, Willing to Relocate: ${this.willingToRelocate}, Remote Work Preference: ${this.remoteWorkPreference ? "Yes" : "No"}, Soft Skills: ${this.softSkills.join(", ")}, Open to Internships: ${this.openToInternships ? "Yes" : "No"}`;
     };
 }
 
 // Creating an Object Using the Applicant Constructor
-const liam = new ApplicantDetails("Liam O'Reily", "Halifax, NS", "Marine Technician", 4, "Diploma in Marine Technology, Nova Scotia Community College"["Sonar operation", "Engine repair", "Navigation systems"], ["Marine Safety Training"], ["English"], null, true, false, ["Teamwork", "Adaptability"], false
+const liam = new ApplicantDetails("Liam O'Reily", "Halifax, NS", "Marine Technician", 4, "Diploma in Marine Technology, Nova Scotia Community College",["Sonar operation", "Engine repair", "Navigation systems"], ["Marine Safety Training"], ["English"], null, true, false, ["Teamwork", "Adaptability"], false
 );
 
-const elem = document.getElementById('output');
+output.textContent = liam.describe();
 
